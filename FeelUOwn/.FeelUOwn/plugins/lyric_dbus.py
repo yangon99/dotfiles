1cddd9d85f5e9b4697e02d083b97556696542eb9
yangon99 <yangon99_all@outlook.com>
2024-05-23 21:16:39 +0800

    upd feeluown lyric_dbus

--- FeelUOwn/.FeelUOwn/plugins/lyric_dbus.py
+++ FeelUOwn/.FeelUOwn/plugins/lyric_dbus.py
@@ -4,7 +4,7 @@ from feeluown.consts import SONG_DIR as DEFAULT_DOWNLOAD_DIR
 from feeluown.fuoexec.signal_manager import signal_mgr
 
 """
-监听 app.live_lyric.sentence_changed 事件，发送dbus信息
+监听 app.live_lyric.lyrics_changed 事件，发送dbus信息
 """
 
 
@@ -16,25 +16,29 @@ __version__ = "0.1"
 logger = logging.getLogger(__name__)
 diy_bar = None
 
-
-def send_dbus_msg(curr_sentence):
+def send_dbus_msg(new_line):
+    logger.debug('get origin new_line: {}'.format(new_line.origin))
     global diy_bar
     if not diy_bar:
         logger.warning('try to get diy_bar object.')
         diy_bar = SessionBus().get_object('org.kde.plasma.doityourselfbar', '/id_1').get_dbus_method('pass')
-        return
     msg_str = ''
-    if curr_sentence is not None and curr_sentence != "":
-        msg_str = '|A|%s|||' % (curr_sentence)
+
+    if new_line.origin is not None and new_line.origin != "":
+        msg_str = '|A|<font-size="%ipx">%s</font>|||' % (16 if new_line.has_trans else 18, new_line.origin)
     else:
         msg_str = '| A | …… |||'
-    logger.info('send dbus message: {}'.format(msg_str))
+    
+    if new_line.has_trans:
+        msg_str += '|B|<font-size="14px">%s</font>|||' % (new_line.trans)
+
+    logger.debug('send dbus message: {}'.format(msg_str))
     diy_bar(msg_str)
 
 
 def enable(app):
-    app.live_lyric.sentence_changed.connect(send_dbus_msg)
+    app.live_lyric.line_changed.connect(send_dbus_msg)
 
 
 def disable(app):
-    signal_mgr.remove('app.live_lyric.sentence_changed', send_dbus_msg, False)
+    app.live_lyric.line_changed.disconnect(send_dbus_msg)
